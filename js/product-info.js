f0c2b979f9430e971df3eb6a73f092f8eb90e24a
Andrea_Aberasturi <aberasturiandrea@gmail.com>
2021-09-16 23:09:13 -0300

    push

--- js/product-info.js
+++ js/product-info.js
@@ -59,12 +59,28 @@ function voirComment(array) {
           <hr class'my-3'>
              <div class="d-flex w-100 justify-content-between">
                 <p> ${comment.description} </p></hr>
-                <p> ${comment.score} </p>
+                <p> ${stars(comment.score)} </p>
              </div>
                  <p class="text-end"> ${comment.user}  &emsp;  ${comment.dateTime} </p>
           `
         document.getElementById('comentarios').innerHTML = voir;
     }
+}
+
+function stars(star) {
+    let score = parseInt(star);
+    let agregar = '';
+    for (let index = 1; index <= score; index++) {
+         agregar = `
+         <span id="1estrella" style="cursor:pointer" class="fa fa-star checked"></span>
+         `
+    }
+    for (let i = score+1; i <=5; i++) {
+         agregar = `
+         <span id="4estrella" style="cursor: pointer" class="fa fa-star"></span>
+         `
+    }
+    return agregar;
 }
 
 //Capturo el comentario e inserto en html
@@ -86,26 +102,6 @@ function commentrio() {
                             ${usuario} &emsp; ${year}-${month}-${day} ${heur}:${minuit}`
 
 }
-
-//Creo la funcion estrellitas
-
-var count = '';
-
-function calificar(item) {
-    console.log(item);
-    count = item.id[0];//   Capturo el primer caracter
-    let name = item.id.substring(1); //Capturo todo menos el priemr caracter
-    
-    for (let i=0; i<5; i++){
-        if (i<count){
-            document.getElementById((i+1)+nombre).style.color= 'orange';
-        } else{
-            document.getElementById((i+1)+nombre).style.color= 'black';
-
-        }
-    }
-}
-
 
 
 var arraysCars = [];
